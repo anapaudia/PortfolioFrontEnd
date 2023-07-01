bcbc8f1a2074da9697d1a0a4662bd16d305e9ee2
anapaudia <paulandiarena@hotmail.com>
2023-06-30 22:55:23 -0300

    Actualizacion

---
+++ src/app/components/experiencia/new-experiencia.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-experiencia',
+  templateUrl: './new-experiencia.component.html',
+  styleUrls: ['./new-experiencia.component.css']
+})
+export class NewExperienciaComponent {
+
+}
